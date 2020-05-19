02a16d8c8b89e18f7b892237fb941659a1c59fea
Jashuva Devireddy <jashuva52@gmail.com>
2020-05-19 11:41:25 +0530

    added multiple select and delete feature

--- ReactTable/ReactTable/src/components/DisplayListWithReactTable.tsx
+++ ReactTable/ReactTable/src/components/DisplayListWithReactTable.tsx
@@ -4,7 +4,7 @@ import { IInputs } from '../../generated/ManifestTypes';
 import {IContact} from '../models/IContact';
 import {Column} from 'react-table';
 import RenderReactTable from './RenderReactTable';
-import {IconButton, Checkbox} from "office-ui-fabric-react";
+import {IconButton, Checkbox,PrimaryButton} from "office-ui-fabric-react";
 
 
 
@@ -34,6 +34,7 @@ class DisplayList extends React.Component<DisplayListProps,DisplayListState>{
     componentDidMount(){
       var columns  = [
         {
+          width : 45,
           Header : <Checkbox 
                       onChange = {(event ,selected) => {
                                            if(selected != undefined){
@@ -53,24 +54,39 @@ class DisplayList extends React.Component<DisplayListProps,DisplayListState>{
                                             }
                                         }
                                         else{
-                                          alert(false);
                                           this.setState({allselected : false,selectedrows : this.state.selectedrows.filter(x => x != (props.original as IContact).contactid)})
                                         }
                                       }
                                   }
                           checked = {this.state.selectedrows.find(x => x == (props.original as IContact).contactid) != undefined ? true : this.state.allselected }
-                    />
+                      />
+          
         },
         {
           Header : "Delete",
           accessor : "contactid",
           id : "",
+          width : 65,
           Cell : props => <IconButton 
                             iconProps ={{iconName : "delete"}}
                               onClick = {() => {
-                              var selectedrecord = props.original as IContact
-                              var remainingrecords = this.state.rows.filter(x => x.contactid != selectedrecord.contactid )
-                              this.setState({rows : remainingrecords})
+                                var selectedrecord = props.original as IContact
+                                this.props.context.webAPI.deleteRecord("contact",selectedrecord.contactid).then(
+                                  () => {
+                                      this.props.context.webAPI.retrieveMultipleRecords("contact").then(
+                                        (response) => {
+                                          var GetRecords = () : IContact[] =>{
+                                            var contacts  = [] as IContact[];
+                                            response.entities.map(x => contacts.push({firstname: x.firstname,fullname: x.fullname,middlename:x.middlename,lastname:x.lastname,contactid:x.contactid} as IContact) );
+                                            return contacts;
+                                          }    
+                                          this.setState({columns:columns,rows:GetRecords()})
+                                        }
+                                      )
+                                  }
+                                )
+                                //var remainingrecords = this.state.rows.filter(x => x.contactid != selectedrecord.contactid )
+                                //this.setState({rows : [...remainingrecords]})
 
                             }}
                           />
@@ -96,7 +112,7 @@ class DisplayList extends React.Component<DisplayListProps,DisplayListState>{
      
       var GetRecords = () : IContact[] => {
         var contacts = [] as IContact[]
-        for(var i=0; i<100; i++){
+        for(var i=0; i<10; i++){
           contacts.push({
             contactid : i.toString(),
             firstname : "Jashuva",
@@ -108,21 +124,21 @@ class DisplayList extends React.Component<DisplayListProps,DisplayListState>{
         }
         return contacts;
       }
-      this.setState({
-        columns : columns,
-        rows : GetRecords()
+      // this.setState({
+      //   columns : columns,
+      //   rows : GetRecords()
         
-      });
-      // this.props.context.webAPI.retrieveMultipleRecords("contact").then(
-      //   (response) => {
-      //     var GetRecords = () : IContact[] =>{
-      //       var contacts  = [] as IContact[];
-      //       response.entities.map(x => contacts.push({firstname: x.firstname,fullname: x.fullname,middlename:x.middlename,lastname:x.lastname,contactid:x.contactid} as IContact) );
-      //       return contacts;
-      //     }    
-      //     this.setState({columns:columns,rows:GetRecords()})
-      //   }
-      // )
+      // });
+      this.props.context.webAPI.retrieveMultipleRecords("contact").then(
+        (response) => {
+          var GetRecords = () : IContact[] =>{
+            var contacts  = [] as IContact[];
+            response.entities.map(x => contacts.push({firstname: x.firstname,fullname: x.fullname,middlename:x.middlename,lastname:x.lastname,contactid:x.contactid} as IContact) );
+            return contacts;
+          }    
+          this.setState({columns:columns,rows:GetRecords()})
+        }
+      )
     }
     public render() : JSX.Element{
       debugger;
@@ -130,15 +146,61 @@ class DisplayList extends React.Component<DisplayListProps,DisplayListState>{
       const columns = this.state.columns;
       
       return (
-        
-          <RenderReactTable 
-            columns = {...columns} 
-            rows = {...records}
-          />
+          <div className="headerDiv">
+            
+            <div className="headerButtonDiv">
+                <PrimaryButton
+                  className="headerButton"
+                  text="Delete"
+                  onClick={() => {
+                    // var remainingrows = [] as IContact[];
+                    // this.state.rows.forEach(x => {
+                    //   debugger;
+                    //     var selectedrecord = this.state.selectedrows.find(y => y == x.contactid);
+                    //     if(selectedrecord == undefined || selectedrecord == null){
+                    //       remainingrows.push(x);
+                    //     }
+                    // })
+                    // this.setState({
+                    //   rows : [...remainingrows]
+                    // })
+                    var deletedcount = 0;
+                    var totalcount = this.state.selectedrows.length;
+                    this.state.selectedrows.forEach(x => {
+                      this.props.context.webAPI.deleteRecord("contact",x).then(
+                        (resp) => {
+                          deletedcount++;
+                          if(deletedcount == totalcount){
+                            this.props.context.webAPI.retrieveMultipleRecords("contact").then(
+                              (response) => {
+                                var GetRecords = () : IContact[] =>{
+                                  var contacts  = [] as IContact[];
+                                  response.entities.map(x => contacts.push({firstname: x.firstname,fullname: x.fullname,middlename:x.middlename,lastname:x.lastname,contactid:x.contactid} as IContact) );
+                                  return contacts;
+                                }    
+                                this.setState({allselected: false,selectedrows: [],columns:columns,rows:GetRecords()})
+                              }
+                            )
+                          }
+                        }
+                      )
+                    })
+                  }}
+                />
+            </div>
+            <br></br><br></br>
+            <div>
+              <RenderReactTable 
+                columns = {...columns} 
+                rows = {...records}
+              />
+            </div>
+          </div>
       
         
       );
     }
+    
 }
 
 export default DisplayList;
