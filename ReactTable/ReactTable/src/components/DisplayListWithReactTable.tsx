6c591e3776461770bdb65b987aa59f66eed382f1
Jashuva Devireddy <jashuva52@gmail.com>
2020-05-22 11:32:51 +0530

    added edit inline and edit multiple features

--- ReactTable/ReactTable/src/components/DisplayListWithReactTable.tsx
+++ ReactTable/ReactTable/src/components/DisplayListWithReactTable.tsx
@@ -4,8 +4,10 @@ import { IInputs } from '../../generated/ManifestTypes';
 import {IContact} from '../models/IContact';
 import {Column} from 'react-table';
 import RenderReactTable from './RenderReactTable';
-import {IconButton, Checkbox,PrimaryButton,DefaultButton} from "office-ui-fabric-react";
+import {IconButton, Checkbox,PrimaryButton,DefaultButton, TextField, IconNames, Panel} from "office-ui-fabric-react";
+import { TooltipHost, ITooltipHostStyles } from 'office-ui-fabric-react/lib/Tooltip';
 import { Dialog, DialogType, DialogFooter } from 'office-ui-fabric-react/lib/Dialog';
+import Loader from 'react-loader-spinner';
 
 
 
@@ -13,13 +15,17 @@ interface DisplayListProps {
     context : ComponentFramework.Context<IInputs>
 }
 interface DisplayListState {
+    multipleeditrecord : any
+    openmultipleedit : boolean
+    loadingtext : string
+    editablerecord : IContact | null,
     columns : Column[],
     rows : IContact[],
     selectedrows : string[]
     allselected : boolean,
     oneunselected : boolean,
     opendeleteconfirmation : boolean
-    opendeletemultiple : boolean
+    openloading : boolean
 }
 
 class DisplayList extends React.Component<DisplayListProps,DisplayListState> {
@@ -27,16 +33,20 @@ class DisplayList extends React.Component<DisplayListProps,DisplayListState> {
     constructor(props : DisplayListProps){
         super(props);
         this.state = {
+            multipleeditrecord : {} as any,
+            openmultipleedit : false,
+            editablerecord : null,
             oneunselected : false,
             allselected : false,
             selectedrows : [],
             columns : [],
             rows : [],
             opendeleteconfirmation : false,
-            opendeletemultiple : false
+            openloading : false,
+            loadingtext : ""
         }
     }
-    GetConstantColumns(estate : DisplayListState) { 
+    public GetConstantColumns = (estate : DisplayListState) => { 
       return [
                   {
                     id : "checkboxcolumn",
@@ -46,13 +56,14 @@ class DisplayList extends React.Component<DisplayListProps,DisplayListState> {
                                                     if(selected != undefined){
                                                       var temp = [] as string[]
                                                       this.state.rows.forEach(x => temp.push(x.contactid));
-                                                      this.setState({allselected : selected,selectedrows : selected ? temp : []})
+                                                      this.setState({allselected : selected,selectedrows : selected ? temp : [],editablerecord : null})
                                                     }
                                                   }}
-                                checked = {estate.allselected  && estate.rows.length > 0}
+                                checked = {this.state.allselected  && estate.rows.length > 0}
                             />,
                     Cell : props => <Checkbox 
                                     onChange = {(x,y) => {
+                                                  this.setState({editablerecord : null})
                                                   if(y){
                                                       var selected = this.state.selectedrows.find(x => x == (props.original as IContact).contactid);
                                                       if(selected == undefined || selected == null){
@@ -66,6 +77,71 @@ class DisplayList extends React.Component<DisplayListProps,DisplayListState> {
                                             }
                                     checked = {this.state.selectedrows.find(x => x == (props.original as IContact).contactid) != undefined ? true : this.state.allselected }
                                 />
+                },
+                {
+                  id : "editbutton",
+                  width : 90,
+                  Cell : (props : any) => { 
+                    return (this.state.editablerecord == null || this.state.editablerecord.contactid != props.original.contactid) ? (
+                      <TooltipHost
+                        content="edit"
+                        id={"tooltipid"}
+                      >
+                    <IconButton 
+                      iconProps = {{iconName : "edit"}} 
+                      onClick = {
+                        ()=>{
+                          this.setState({editablerecord :{...props.original} as IContact });
+                        }
+                      }
+                    />
+                    </TooltipHost>) : 
+                    (
+                      <div>
+                        <TooltipHost
+                            content="save"
+                            id={"tooltipid"}
+                        >
+                        <IconButton 
+                            iconProps = {{iconName : "save"}} 
+                            onClick = {
+                              () => {
+                                let rowsL = this.state.rows;
+                                for(var i =0;i<rowsL.length;i++){
+                                    if(rowsL[i].contactid == this.state.editablerecord?.contactid){
+                                      rowsL[i] = this.state.editablerecord;
+                                    }
+                                }
+                                this.setState({
+                                openloading : true,
+                                loadingtext : "Saving Data"
+                                })
+                                this.props.context.webAPI.updateRecord("contact",this.state.editablerecord!.contactid,this.state.editablerecord!).then(
+                                  () => {
+                                    this.setState({openloading : false,editablerecord : null,rows : rowsL})
+                                  }
+                                )
+                              }
+                            } 
+                        />
+                        </TooltipHost>
+                        <span>
+                        <TooltipHost 
+                          content = "cacel"
+                        >
+                        <IconButton 
+                            iconProps = {{iconName : "cancel"}} 
+                            onClick = {
+                              () => {
+                                this.setState({editablerecord : null,rows : this.state.rows})
+                              }
+                            } 
+                        />
+                        </TooltipHost>
+                        </span>
+                      </div>  
+                    )
+                  }
                 }
             ] as Column[]
     }
@@ -73,19 +149,75 @@ class DisplayList extends React.Component<DisplayListProps,DisplayListState> {
       let columnsL  = [
         {
         Header : "First Name",
-        accessor : "firstname"
+        accessor : "firstname",
+        Cell : (props : any) => {
+            return this.state.editablerecord?.contactid == props.original.contactid ? 
+              <TextField 
+                defaultValue = {props.original.firstname} 
+                underlined = {true} 
+                onChange = {
+                  (ev,value) => { 
+                          let updatablerecord = this.state.editablerecord;
+                          updatablerecord!.firstname = value!;
+                          this.setState({editablerecord : this.state.editablerecord})}
+                        }
+              />
+              : <div>{props.original.firstname}</div> 
+          }
         },
         {
           Header : "Last Name",
-          accessor : "lastname"
+          accessor : "lastname",
+          Cell : (props : any) => {
+            return this.state.editablerecord?.contactid == props.original.contactid ? 
+              <TextField 
+                defaultValue = {props.original.lastname} 
+                underlined = {true} 
+                onChange = {
+                  (ev,value) => { 
+                          let updatablerecord = this.state.editablerecord;
+                          updatablerecord!.lastname = value!;
+                          this.setState({editablerecord : this.state.editablerecord})}
+                        }
+              />
+              : <div>{props.original.lastname}</div> 
+          }
         },
         {
           Header : "Middle Name",
-          accessor : "middlename"
+          accessor : "middlename",
+          Cell : (props : any) => {
+            return this.state.editablerecord?.contactid == props.original.contactid ? 
+              <TextField 
+                defaultValue = {props.original.middlename} 
+                underlined = {true} 
+                onChange = {
+                  (ev,value) => { 
+                          let updatablerecord = this.state.editablerecord;
+                          updatablerecord!.middlename = value!;
+                          this.setState({editablerecord : this.state.editablerecord})}
+                        }
+              />
+              : <div>{props.original.middlename}</div> 
+          }
         },
         {
           Header : "Full Name",
-          accessor : "fullname"
+          accessor : "fullname",
+          Cell : (props : any) => {
+            return this.state.editablerecord?.contactid == props.original.contactid ? 
+              <TextField 
+                defaultValue = {props.original.fullname} 
+                underlined = {true} 
+                onChange = {
+                  (ev,value) => { 
+                          let updatablerecord = this.state.editablerecord;
+                          updatablerecord!.fullname = value!;
+                          this.setState({editablerecord : this.state.editablerecord})}
+                        }
+              />
+              : <div>{props.original.fullname}</div> 
+          }
         }
       ] as Column[]
      
@@ -104,11 +236,10 @@ class DisplayList extends React.Component<DisplayListProps,DisplayListState> {
         return contacts;
       }
       // this.setState({
-      //   columns : columns,
+      //   columns : columnsL,
       //   rows : GetRecords()
         
       // });
-      
       this.props.context.webAPI.retrieveMultipleRecords("contact").then(
         (response) => {
           let GetRecords = () : IContact[] =>{
@@ -120,6 +251,7 @@ class DisplayList extends React.Component<DisplayListProps,DisplayListState> {
         }
       )
     }
+    
     public render() : JSX.Element{
       debugger;
       const dialogContentProps = {
@@ -128,12 +260,79 @@ class DisplayList extends React.Component<DisplayListProps,DisplayListState> {
         closeButtonAriaLabel: 'Close',
         subText: 'Are you sure, you want to delete ?',
       };
+      
       let records = [...this.state.rows];
-      let columns = [...this.GetConstantColumns(this.state),...(this.state.columns).filter(x=>x.id != "checkboxcolumn" || x.id == null || x.id == undefined)]
+      let constantcolumns = this.GetConstantColumns(this.state);
+      if(this.state.selectedrows.length !=0)
+        constantcolumns = constantcolumns.filter(x => x.id != "editbutton");
+      let columns = [...constantcolumns,...(this.state.columns).filter(x=>(x.id != "checkboxcolumn" && x.id != "savebutton" && x.id != "editbutton") || x.id == null || x.id == undefined)]
       return (
+          
           <div className="headerDiv">
-            
+            <div>
+            {this.state.openmultipleedit && 
+              <Panel
+                isOpen = {this.state.openmultipleedit}
+                onDismiss = {() => {this.setState({openmultipleedit : false})}}
+                headerText  = "Hello"
+                closeButtonAriaLabel = "close"
+                isFooterAtBottom = {true}
+                onRenderFooter = {
+                  () => 
+                  <div style={{marginLeft : 15}}>
+                    <PrimaryButton text="save" 
+                      styles={{root : {marginRight : 8}}}
+                      onClick = {
+                        ()=> {
+                        //alert(this.state.multipleeditrecord.firstname)
+                        let keys = Object.keys(this.state.multipleeditrecord);
+                        let rowsl = [...this.state.rows]
+                        let count = 0;
+                        this.state.selectedrows.forEach(x => {
+                          
+                          let selectedrow = rowsl.find(y=>y.contactid == x) as any
+                          keys.forEach(k => selectedrow[k] = this.state.multipleeditrecord[k])
+                          var index = rowsl.findIndex(a=>a.contactid == x)
+                          rowsl[index] = selectedrow as IContact
+                          this.setState({openloading : true,loadingtext : "Saving"})
+                          this.props.context.webAPI.updateRecord("contact",rowsl[index].contactid,rowsl[index]).then(
+                            (resp) => {
+                              count++
+                              if(count == this.state.selectedrows.length){
+                                this.setState({rows : rowsl,openmultipleedit : false,openloading : false,loadingtext : ""})
+                              }
+                            }
+                            )
+                        })
+                        this.setState({openmultipleedit : false,rows : rowsl})
+                      }}
+                    />
+                    <DefaultButton text="cancel" 
+                      onClick = {() => {this.setState({openmultipleedit : false})}}
+                    />
+                  </div>}
+              >
+              {this.state.columns.map(
+               (x) => 
+               <div>
+                 <TextField label={x.Header?.toString()} 
+                  onChange = {(ev,value) => {this.setState({multipleeditrecord : {...this.state.multipleeditrecord, [(x.accessor) as string] : value}})}}
+                 />
+                </div>
+              )}
+              </Panel>
+            }
+            </div>
             <div className="headerButtonDiv">
+                <PrimaryButton
+                  className="headerButton"
+                  text="Edit"
+                  disabled = {this.state.selectedrows.length == 0}
+                  onClick={() => {
+                    this.setState({openmultipleedit : true})
+                    
+                  }}
+                />
                 <PrimaryButton
                   className="headerButton"
                   text="Delete"
@@ -160,11 +359,24 @@ class DisplayList extends React.Component<DisplayListProps,DisplayListState> {
               <DialogFooter>
                 <PrimaryButton onClick={()=> {
                   this.setState({
-                    opendeletemultiple : true,
+                    openloading : true,
+                    loadingtext : "Deleting",
                     opendeleteconfirmation : false
                   })
-                   var deletedcount = 0;
+                    var deletedcount = 0;
                     var totalcount = this.state.selectedrows.length;
+                  // var remaining = [] as IContact[]
+                  //  this.state.rows.forEach((x) => {
+                  //       let selectedrecord = this.state.selectedrows.find(y => y == x.contactid)
+                  //       if(selectedrecord == undefined || selectedrecord == null)
+                  //           remaining.push(x);
+                  //     }
+                  //  )
+                  //  this.setState({
+                  //    rows : [...remaining],
+                  //    opendeletemultiple : false,
+                  //    selectedrows : []
+                  //  })
                     this.state.selectedrows.forEach(x => {
                       this.props.context.webAPI.deleteRecord("contact",x).then(
                         (resp) => {
@@ -177,7 +389,7 @@ class DisplayList extends React.Component<DisplayListProps,DisplayListState> {
                                   response.entities.map(x => contacts.push({firstname: x.firstname,fullname: x.fullname,middlename:x.middlename,lastname:x.lastname,contactid:x.contactid} as IContact) );
                                   return contacts;
                                 }    
-                                this.setState({opendeletemultiple : false,allselected: false,selectedrows: [],columns:columns,rows:GetRecords()})
+                                this.setState({openloading : false,allselected: false,selectedrows: [],columns:columns,rows:GetRecords()})
                               }
                             )
                           }
@@ -188,14 +400,29 @@ class DisplayList extends React.Component<DisplayListProps,DisplayListState> {
                 <DefaultButton onClick={()=> {this.setState({opendeleteconfirmation:false})}} text="No" />
               </DialogFooter>
             </Dialog>
+            {
+            this.state.openloading && 
             <Dialog 
-              hidden = {!this.state.opendeletemultiple}
+              hidden = {!this.state.openloading}
               dialogContentProps = {{
                 type: DialogType.normal,
-                title: 'Deleting....',
+                title: this.state.loadingtext,
                 closeButtonAriaLabel: 'Close',
                 }}
-            />
+            > 
+            <div>
+              <div className={"loadingData"}>
+                <Loader
+                  type="Oval"
+                  key="Loading"
+                  color="#00BFFF"
+                  height={100}
+                  width={100}
+                />
+              </div>
+          </div>
+            </Dialog>
+            }
             </div>
           </div>
       
