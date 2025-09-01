27f6b4b8cbb79aaf07b08ac050a30042e8859ff2
Jashuva Devireddy <jashuva52@gmail.com>
2025-09-01 22:40:19 +0530

    Added AssignmentPeople Subgrid control

---
+++ AssignedPeopleSubgrid/AssignedPeopleSubgrid/AssignedPeople.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import { IInputs } from "./generated/ManifestTypes";
+import { DetailsList, IColumn } from "@fluentui/react";
+
+interface AssignedPeopleProps {
+    context : ComponentFramework.Context<IInputs>
+}
+interface AssignedPeopleState {
+    columns : IColumn[],
+    items : any[]
+}
+
+class AssignedPeople extends React.Component<AssignedPeopleProps,AssignedPeopleState> {
+    constructor(props : AssignedPeopleProps){
+        super(props);
+        this.state = {
+            items : [],
+            columns : []
+        }
+    }
+    componentDidMount(): void {
+        var columns = this.props.context.parameters.sampleDataSet.columns.map(x => {
+            debugger;
+            return x;
+        });
+        var records = Object.keys(this.props.context.parameters.sampleDataSet.records).map(x => {
+            debugger;
+            var eachrecord = {} as any;
+            var data = this.props.context.parameters.sampleDataSet.records[x];
+            columns.forEach(y => {
+                eachrecord[y.name] = data.getFormattedValue(y.name);
+            });
+            return eachrecord;
+        });
+        this.setState({
+            columns : columns.map(x => {
+                return {
+                    name : x.displayName,
+                    fieldName : x.name,
+                    minWidth : 60,
+                    maxWidth : 60
+                } as IColumn
+            }),
+            items : records
+        });
+    }
+    render(): React.ReactNode {
+        return <div>
+            Hello, this line is added from the custom control. {Object.keys(this.props.context.parameters.sampleDataSet.records).length}
+            <DetailsList
+                items={this.state.items}
+                columns = {this.state.columns}
+            />
+        </div>
+    }
+}
+
+export default AssignedPeople;
