1cd21298cbe2271ef137237db0132df33b08f98b
Jashuva Devireddy <jashuva52@gmail.com>
2026-01-13 02:20:10 +0530

    appfields_version2

--- ApplicationFields/ApplicationFields/HostingDataCenterMigratedFrom.tsx
+++ ApplicationFields/ApplicationFields/HostingDataCenterMigratedFrom.tsx
@@ -17,7 +17,7 @@ class HostingDataCenterMigratedFrom extends React.Component<HostingDataCenterMig
     }
     componentDidMount() {
         var recordid = (this.props.context as any).page.entityId;
-        this.props.context.webAPI.retrieveMultipleRecords("cr549_appdcxwalk", `?$select=_cr549_data_center_name_value$filter=_cr549_app_name_value eq ${recordid}`).then(
+        this.props.context.webAPI.retrieveMultipleRecords("cr549_appdcxwalk", `?$select=_cr549_data_center_name_value&$filter=_cr549_app_name_value eq ${recordid}`).then(
             (result) => {
                 if(result.entities.length > 0 && result.entities[0]["_cr549_data_center_name_value"]){
                      this.setState({ value: result.entities[0]["_cr549_data_center_name_value@OData.Community.Display.V1.FormattedValue"] });
@@ -31,7 +31,7 @@ class HostingDataCenterMigratedFrom extends React.Component<HostingDataCenterMig
 
     render(): React.ReactNode {
         return <>
-            <TextField label='Hosting Data Center Migrating/ed From' value={this.state.value}></TextField>
+            <TextField value={this.state.value}></TextField>
         </>
     }  
 } 
