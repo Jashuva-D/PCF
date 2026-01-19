f38f20b117293c1b53a7318b88a76a3d1ee17687
Jashuva Devireddy <jashuva52@gmail.com>
2026-01-20 00:56:59 +0530

    PAGridCustomiser_version 2

--- PowerAppsGridCustomizerControl/PAGridCustomizer/customizers/CellRendererOverrides.tsx
+++ PowerAppsGridCustomizerControl/PAGridCustomizer/customizers/CellRendererOverrides.tsx
@@ -5,10 +5,10 @@ import { CellRendererProps, CellRendererOverrides } from "../types";
 export const cellRendererOverrides: CellRendererOverrides = {
 	["Text"]: (props: CellRendererProps, col) => {
 		if (col.colDefs[col.columnIndex].name === "cr549_id") {
-			return <Text style={{backgroundColor:"red", color:"white"}}>{props.formattedValue}</Text>
+			return <div style={{backgroundColor: "red", width: "100%", height: "100%"}}><Text style={{backgroundColor:"red", color:"white"}}>{props.formattedValue}</Text></div>;
 		}
 		else if(col.colDefs[col.columnIndex].name === "cr549_cms_office"){
-			return <Text style={{ backgroundColor: "blue", color: "white" }}>{props.formattedValue}</Text>;
+			return <div style={{ backgroundColor: "blue", width: "100%", height: "100%"}}><Text style={{ backgroundColor: "blue", color: "white" }}>{props.formattedValue}</Text></div>;
 		}
 	}
 };
