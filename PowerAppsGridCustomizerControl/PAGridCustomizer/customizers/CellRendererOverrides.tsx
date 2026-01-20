0b38a9923f82dc597cfafea4dc47606f85ec82f3
Jashuva Devireddy <jashuva52@gmail.com>
2026-01-20 22:29:52 +0530

    PAGridCustomizer_version 5

--- PowerAppsGridCustomizerControl/PAGridCustomizer/customizers/CellRendererOverrides.tsx
+++ PowerAppsGridCustomizerControl/PAGridCustomizer/customizers/CellRendererOverrides.tsx
@@ -1,15 +1,14 @@
-import { Label, Text } from "@fluentui/react";
+import { Label, Text, Stack } from "@fluentui/react";
 import * as React from "react";
 import { CellRendererProps, CellRendererOverrides } from "../types";
 
 export const cellRendererOverrides: CellRendererOverrides = {
 	["Text"]: (props: CellRendererProps, col) => {
 		if (col.colDefs[col.columnIndex].name === "cr549_cms_group") {
-			//return <div style={{backgroundColor: "#D3DAE2", width: "100%", height: "100%"}}><Text>{props.formattedValue}</Text></div>;
-			return <Text style={{backgroundColor: "#D3DAE2", padding: "4px"}}>{props.formattedValue}</Text>;
+			return <Stack verticalAlign="center" horizontalAlign="start"><Text style={{backgroundColor: "#EEEAF4", padding: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
 		}
 		else if(col.colDefs[col.columnIndex].name === "cr549_cms_office"){
-			return <Text style={{backgroundColor: "#D8D2E8", padding: "4px"}}>{props.formattedValue}</Text>;
+			return <Stack verticalAlign="center" horizontalAlign="start"><Text style={{backgroundColor: "#E9EEF3", padding: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
 		}
 	}
 };
