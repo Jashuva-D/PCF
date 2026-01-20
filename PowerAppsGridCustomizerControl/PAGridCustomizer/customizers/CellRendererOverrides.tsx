116ea9fee9ce34643d7c096193447808a07bf4bc
Jashuva Devireddy <jashuva52@gmail.com>
2026-01-20 21:42:40 +0530

    PAGridCustomizer_version 3

--- PowerAppsGridCustomizerControl/PAGridCustomizer/customizers/CellRendererOverrides.tsx
+++ PowerAppsGridCustomizerControl/PAGridCustomizer/customizers/CellRendererOverrides.tsx
@@ -4,11 +4,11 @@ import { CellRendererProps, CellRendererOverrides } from "../types";
 
 export const cellRendererOverrides: CellRendererOverrides = {
 	["Text"]: (props: CellRendererProps, col) => {
-		if (col.colDefs[col.columnIndex].name === "cr549_id") {
-			return <div style={{backgroundColor: "red", width: "100%", height: "100%"}}><Text style={{backgroundColor:"red", color:"white"}}>{props.formattedValue}</Text></div>;
+		if (col.colDefs[col.columnIndex].name === "cr549_cms_group") {
+			return <div style={{backgroundColor: "#D3DAE2", width: "100%", height: "100%"}}><Text style={{backgroundColor:"red", color:"white"}}>{props.formattedValue}</Text></div>;
 		}
 		else if(col.colDefs[col.columnIndex].name === "cr549_cms_office"){
-			return <div style={{ backgroundColor: "blue", width: "100%", height: "100%"}}><Text style={{ backgroundColor: "blue", color: "white" }}>{props.formattedValue}</Text></div>;
+			return <div style={{ backgroundColor: "#D8D2E8", width: "100%", height: "100%"}}><Text style={{ backgroundColor: "blue", color: "white" }}>{props.formattedValue}</Text></div>;
 		}
 	}
 };
