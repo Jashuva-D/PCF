327df98f5dbf3551508774a23c06e6628cb08a0d
Jashuva Devireddy <jashuva52@gmail.com>
2026-01-31 00:54:43 +0530

    version 6

--- AppUserRoles/AppUserRoles/AppUserRoles.tsx
+++ AppUserRoles/AppUserRoles/AppUserRoles.tsx
@@ -18,32 +18,40 @@ class AppUserRoles extends React.Component<AppUserRolesProps, AppUserRolesState>
                 cols.push({
                     key: c.name,
                     name: c.displayName,
-                    fieldName: c.name,
+                    fieldName: c.name.replace("a_0bbe2879d1e8f0118544001dd8096c2b","")
                 } as IColumn);
         });
-        cols.push({
-            key: "cr549_person",
-            name: "Person",
-            fieldName: "cr549_person",
-        } as IColumn);
-        cols.push({
-            key: "cr549_role",
-            name: "Role",
-            fieldName: "cr549_role",
-        } as IColumn);
-        cols.push
+        
         this.state = {
             columns: cols,
             items: []
         }
     }
     componentDidMount(): void {
+        // var fetchxml = `<fetch version="1.0" output-format="xml-platform" mapping="logical" distinct="false">
+        //         <entity name="cr549_appuserrole">
+        //         <attribute name="cr549_person"/>
+        //         <attribute name="cr549_appuserroleid"/>
+        //         <attribute name="cr549_role"/>
+        //         <order attribute="cr549_person" descending="false"/>
+        //         <filter type="and">
+        //         <condition attribute="cr549_app" operator="eq" uiname="1115 PMDA" uitype="cr549_application" value="{9A5926CF-7DDF-F011-8544-001DD806C085}"/>
+        //         </filter>
+        //         <link-entity name="cr549_person" from="cr549_personid" to="cr549_person" visible="false" link-type="outer" alias="a_0bbe2879d1e8f0118544001dd8096c2b">
+        //         <attribute name="cr549_email_address_2"/>
+        //         <attribute name="cr549_direct_phone"/>
+        //         <attribute name="cr549_email_address"/>
+        //         <attribute name="cr549_service_desk_agent"/>
+        //         <attribute name="cr549_id"/>
+        //         </link-entity>
+        //         </entity>
+        //         </fetch>"`;
         let items: any[] = [];
         this.props.context.parameters.sampleDataSet.sortedRecordIds.forEach((id) => {
             const record = this.props.context.parameters.sampleDataSet.records[id];
             let item: any = {};
-            this.state.columns.forEach((c) => {
-                item[c.name] = record.getValue(c.name);
+            this.state.columns.forEach((c : IColumn) => {
+                item[c.fieldName ?? ""] = record.getValue(c.key);
             });
             items.push(item);
         });
@@ -51,7 +59,7 @@ class AppUserRoles extends React.Component<AppUserRolesProps, AppUserRolesState>
     }
     render(): React.ReactNode {
         return <div>
-                <div>App User Roles Component</div>
+                <div>App User Roles Component1</div>
                  <DetailsList
                     items={[...this.state.items]}
                     columns={[...this.state.columns]}
