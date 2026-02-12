be3576362647b0ee6144afe7610bb5ae66fa29ed
Jashuva Devireddy <jashuva52@gmail.com>
2026-02-13 00:37:22 +0530

    version 20

--- PowerAppsGridCustomizerControl/PAGridCustomizer/customizers/CellRendererOverrides.tsx
+++ PowerAppsGridCustomizerControl/PAGridCustomizer/customizers/CellRendererOverrides.tsx
@@ -5,10 +5,10 @@ import { CellRendererProps, CellRendererOverrides } from "../types";
 export const cellRendererOverrides: CellRendererOverrides = {
 	["Text"]: (props: CellRendererProps, col) => {
 		if (col.colDefs[col.columnIndex].name === "cr549_cms_group" && props.formattedValue != null && props.formattedValue != "") {
-			return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%"}}><Text style={{backgroundColor: "#EBE8CE", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
+			return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "5px"}}><Text style={{backgroundColor: "#EBE8CE", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
 		}
 		else if(col.colDefs[col.columnIndex].name === "cr549_cms_office" && props.formattedValue != null && props.formattedValue != "") {
-			return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%"}}><Text style={{backgroundColor: "#E6C8DB", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
+			return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "5px"}}><Text style={{backgroundColor: "#E6C8DB", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
 		}
 	},
 	["TwoOptions"]: (props: CellRendererProps, col) => {
