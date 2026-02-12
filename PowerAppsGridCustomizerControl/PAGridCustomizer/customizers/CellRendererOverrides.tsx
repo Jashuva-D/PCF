e5be7b733d413f1db6bece2cb2e947f6d5dc15b2
Jashuva Devireddy <jashuva52@gmail.com>
2026-02-13 00:42:06 +0530

    version 21

--- PowerAppsGridCustomizerControl/PAGridCustomizer/customizers/CellRendererOverrides.tsx
+++ PowerAppsGridCustomizerControl/PAGridCustomizer/customizers/CellRendererOverrides.tsx
@@ -5,15 +5,15 @@ import { CellRendererProps, CellRendererOverrides } from "../types";
 export const cellRendererOverrides: CellRendererOverrides = {
 	["Text"]: (props: CellRendererProps, col) => {
 		if (col.colDefs[col.columnIndex].name === "cr549_cms_group" && props.formattedValue != null && props.formattedValue != "") {
-			return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "5px"}}><Text style={{backgroundColor: "#EBE8CE", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
+			return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "8px"}}><Text style={{backgroundColor: "#EBE8CE", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
 		}
 		else if(col.colDefs[col.columnIndex].name === "cr549_cms_office" && props.formattedValue != null && props.formattedValue != "") {
-			return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "5px"}}><Text style={{backgroundColor: "#E6C8DB", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
+			return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "8px"}}><Text style={{backgroundColor: "#E6C8DB", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
 		}
 	},
 	["TwoOptions"]: (props: CellRendererProps, col) => {
 		if (col.colDefs[col.columnIndex].name === "cr549_marketplace") {
-			return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "5px"}}>
+			return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "8px"}}>
 				<Toggle
                   style={{marginLeft: "5px", border: "none"}}
 				          checked={props.value == 1}
