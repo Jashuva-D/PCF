113b7cf2950abe829a1c96d50aba78916036a21f
Jashuva Devireddy <jashuva52@gmail.com>
2026-02-14 01:14:20 +0530

    version 22

--- PowerAppsGridCustomizerControl/PAGridCustomizer/customizers/CellRendererOverrides.tsx
+++ PowerAppsGridCustomizerControl/PAGridCustomizer/customizers/CellRendererOverrides.tsx
@@ -10,55 +10,55 @@ export const cellRendererOverrides: CellRendererOverrides = {
 		else if(col.colDefs[col.columnIndex].name === "cr549_cms_office" && props.formattedValue != null && props.formattedValue != "") {
 			return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "8px"}}><Text style={{backgroundColor: "#E6C8DB", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
 		}
-	},
-	["TwoOptions"]: (props: CellRendererProps, col) => {
-		if (col.colDefs[col.columnIndex].name === "cr549_marketplace") {
-			return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "8px"}}>
-				<Toggle
-                  style={{marginLeft: "5px", border: "none"}}
-				          checked={props.value == 1}
-				          inlineLabel label= {props.value == 1 ? "Yes" : "No"}
-				          styles={{
-                    label: { order: 1 },
-                    root: {
-                      selectors: {
-                        "&:hover .ms-Toggle-thumb": {
-                          backgroundColor: props.value == 1 ? "#C6F6DB" : "#ffffff !important"
-                        }
-                      }
-                    },
-                    thumb: {
-                      backgroundColor: props.value == 1 ? "#C6F6DB" : "#ffffff",
-                      color: "red",
-                      height: 20, width: 20, padding: 1,
-                      selectors: {
-                        ":hover": {
-                          backgroundColor: props.value == 1 ? "#C6F6DB" : "#ffffff"   
-                        },
-
-                        '[aria-checked="true"] &': {
-                          backgroundColor: props.value == 1 ? "#C6F6DB" : "#ffffff"
-                        },
-
-                        '[aria-checked="true"]:hover &': {
-                          backgroundColor: props.value == 1 ? "#C6F6DB" : "#ffffff"   
-                        }
-                      }
-                    },
-                    container: { display: 'flex', flexDirection: 'row-reverse' },
-                    pill: {
-                      backgroundColor: props.value == 1 ? "#7BEBAC" : "rgb(211,211,211)",
-                      height: 24, padding: 0, width: 44, borderRadius: 12,
-                      selectors: {
-                        ':hover': { backgroundColor: props.value == 1 ? "#7BEBAC" : "#e6e6e6" }
-                      },
-                      border: "1px solid",
-                      alignItems: "center"
-                    }
-
-                  }}
-                />
-			</Stack>;
-		}
 	}
+	// ["TwoOptions"]: (props: CellRendererProps, col) => {
+	// 	if (col.colDefs[col.columnIndex].name === "cr549_marketplace") {
+	// 		return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "8px"}}>
+	// 			<Toggle
+  //                 style={{marginLeft: "5px", border: "none"}}
+	// 			          checked={props.value == 1}
+	// 			          inlineLabel label= {props.value == 1 ? "Yes" : "No"}
+	// 			          styles={{
+  //                   label: { order: 1 },
+  //                   root: {
+  //                     selectors: {
+  //                       "&:hover .ms-Toggle-thumb": {
+  //                         backgroundColor: props.value == 1 ? "#C6F6DB" : "#ffffff !important"
+  //                       }
+  //                     }
+  //                   },
+  //                   thumb: {
+  //                     backgroundColor: props.value == 1 ? "#C6F6DB" : "#ffffff",
+  //                     color: "red",
+  //                     height: 20, width: 20, padding: 1,
+  //                     selectors: {
+  //                       ":hover": {
+  //                         backgroundColor: props.value == 1 ? "#C6F6DB" : "#ffffff"   
+  //                       },
+
+  //                       '[aria-checked="true"] &': {
+  //                         backgroundColor: props.value == 1 ? "#C6F6DB" : "#ffffff"
+  //                       },
+
+  //                       '[aria-checked="true"]:hover &': {
+  //                         backgroundColor: props.value == 1 ? "#C6F6DB" : "#ffffff"   
+  //                       }
+  //                     }
+  //                   },
+  //                   container: { display: 'flex', flexDirection: 'row-reverse' },
+  //                   pill: {
+  //                     backgroundColor: props.value == 1 ? "#7BEBAC" : "rgb(211,211,211)",
+  //                     height: 24, padding: 0, width: 44, borderRadius: 12,
+  //                     selectors: {
+  //                       ':hover': { backgroundColor: props.value == 1 ? "#7BEBAC" : "#e6e6e6" }
+  //                     },
+  //                     border: "1px solid",
+  //                     alignItems: "center"
+  //                   }
+
+  //                 }}
+  //               />
+	// 		</Stack>;
+	// 	}
+	// }
 };
