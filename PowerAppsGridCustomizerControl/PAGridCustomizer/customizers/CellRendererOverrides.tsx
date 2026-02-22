b3956d62b80ae3f20709cb1ad1949f8249625c6f
Jashuva Devireddy <jashuva52@gmail.com>
2026-02-22 23:11:23 +0530

    version 26

--- PowerAppsGridCustomizerControl/PAGridCustomizer/customizers/CellRendererOverrides.tsx
+++ PowerAppsGridCustomizerControl/PAGridCustomizer/customizers/CellRendererOverrides.tsx
@@ -10,6 +10,9 @@ export const cellRendererOverrides: CellRendererOverrides = {
 		else if(col.colDefs[col.columnIndex].name === "cr549_cms_office" && props.formattedValue != null && props.formattedValue != "") {
 			return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "8px"}}><Text style={{backgroundColor: "#E6C8DB", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
 		}
+    else if(col.colDefs[col.columnIndex].name.endsWith("cr549_projectnumber") && props.formattedValue != null && props.formattedValue != "") {
+      return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "8px"}}><Text style={{backgroundColor: "#EBE8CE", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
+    }
 	},
 	// ["TwoOptions"]: (props: CellRendererProps, col) => {
 	// 	if (col.colDefs[col.columnIndex].name === "cr549_marketplace") {
