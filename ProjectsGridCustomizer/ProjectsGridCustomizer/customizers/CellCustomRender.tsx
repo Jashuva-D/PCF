9cc39f9ec92e420c8c3172a0bc1ecffa0d61a859
Jashuva Devireddy <jashuva52@gmail.com>
2026-03-02 01:25:32 +0530

    version 2

--- ProjectsGridCustomizer/ProjectsGridCustomizer/customizers/CellCustomRender.tsx
+++ ProjectsGridCustomizer/ProjectsGridCustomizer/customizers/CellCustomRender.tsx
@@ -10,12 +10,14 @@ export const CellCustomRender: CellRendererOverrides = {
         if(col.colDefs[col.columnIndex].name == "cr549_projectnumber" && props.formattedValue != null && props.formattedValue != "") {
             return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "8px"}}><Text style={{backgroundColor: "#f2f2f2", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
         }
-        else if(col.colDefs[col.columnIndex].name == "cr549_hostingprojectid" && props.formattedValue != null && props.formattedValue != "") {
-            return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "8px"}}><Text style={{backgroundColor: "#E6C8DB", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
-        }
         else if(col.colDefs[col.columnIndex].name == "cr549_hostingprojectnumber" && props.formattedValue != null && props.formattedValue != "") {
             return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "8px"}}><Text style={{backgroundColor: "#d8dcee", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
         }
-	}
+	},
+    ["Integer"]: (props: CellRendererProps, col) => {
+        if(col.colDefs[col.columnIndex].name == "cr549_hostingprojectid" && props.formattedValue != null && props.formattedValue != "") {
+            return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "8px"}}><Text style={{backgroundColor: "#E6C8DB", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
+        }
+    }
 };
 
