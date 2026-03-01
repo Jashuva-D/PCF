77f3b348c6bced1f8f37d977a58b7133517300e9
Jashuva Devireddy <jashuva52@gmail.com>
2026-03-02 00:36:24 +0530

    version 7

--- AccountsGridCustomizer/AccountsGridCustomizer/customizers/CellCustomRender.tsx
+++ AccountsGridCustomizer/AccountsGridCustomizer/customizers/CellCustomRender.tsx
@@ -7,19 +7,19 @@ export const CellCustomRender: CellRendererOverrides = {
 		if (col.colDefs[col.columnIndex].name === "cr549_acct_name" && props.formattedValue != null && props.formattedValue != "") {
 			return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "8px"}}><Text style={{backgroundColor: "#EBE8CE", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
 		}
-		else if(col.colDefs[col.columnIndex].name === "cr549_cms_office" && props.formattedValue != null && props.formattedValue != "") {
+		else if(col.colDefs[col.columnIndex].name.includes("cr549_cms_office") && props.formattedValue != null && props.formattedValue != "") {
 			return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "8px"}}><Text style={{backgroundColor: "#E6C8DB", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
 		}
         else if(col.colDefs[col.columnIndex].name.endsWith("cr549_projectnumber") && props.formattedValue != null && props.formattedValue != "") {
             return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "8px"}}><Text style={{backgroundColor: "#EBE8CE", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
-        }//ad_depthtest2
-        // else if(col.colDefs[col.columnIndex].name.endsWith("cr549_projectnumber") && props.formattedValue != null && props.formattedValue != "") {
-        //     return <Stack verticalAlign="center" horizontalAlign="start" style={{height: "100%", paddingLeft: "8px"}}><Text style={{backgroundColor: "#EBE8CE", paddingLeft: "8px", paddingRight: "8px", borderRadius: "4px"}}>{props.formattedValue}</Text></Stack>;
-        // }
+        }
 	},
     ["TwoOptions"]: (props: CellRendererProps, col) => {
-        if (col.colDefs[col.columnIndex].name === "cr549_marketplace") {
+        if (col.colDefs[col.columnIndex].name.includes("cr549_marketplace")) {
             return <Stack verticalAlign="center" horizontalAlign="center" style={{height: "100%", paddingLeft: "8px"}}><Text style={{color: props.value == 1 ? "#12890E" : "#E31C3D"}}>{props.value == 1 ? "Yes" : "No"}</Text></Stack>
+        }
+        else if (col.colDefs[col.columnIndex].name == "cr549_decom_flag" && props.formattedValue != null && props.formattedValue != ""){
+            return <Stack verticalAlign="center" horizontalAlign="center" style={{height: "100%", paddingLeft: "8px"}}><Text style={{color: props.value == 0 ? "#12890E" : "#E31C3D"}}>{props.formattedValue}</Text></Stack>
         }
     }
 };
