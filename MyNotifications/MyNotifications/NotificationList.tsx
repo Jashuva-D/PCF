d62755620086ecfd2c16709de847979e24299901
Jashuva Devireddy <jashuva52@gmail.com>
2026-03-22 01:37:03 +0530

    version 1

---
+++ MyNotifications/MyNotifications/NotificationList.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { Stack, Text, Icon, Label, initializeIcons } from "@fluentui/react";
+
+interface Notification {
+    type: string;
+    message: string;
+    user: string;
+    time: string;
+    color: string;
+}
+
+interface NotificationListProps {
+    notifications: Notification[];
+}
+
+export const NotificationList: React.FC<NotificationListProps> = ({ notifications }) => {
+    initializeIcons();
+    return (
+        <Stack tokens={{ childrenGap: 10 }} styles={{ root: { padding: 20 } }}>
+            {notifications.map((notification, index) => (
+                <Stack
+                    key={index}
+                    horizontal
+                    tokens={{ childrenGap: 10 }}
+                    styles={{ root: { border: `1px solid ${notification.color}`, padding: 10, borderRadius: 5 } }}
+                >
+                    <Icon iconName="Info" styles={{ root: { color: notification.color, fontSize: 20 } }} />
+                    <Stack>
+                        <Text variant="mediumPlus" styles={{ root: { fontWeight: "bold" } }}>
+                            {notification.type}: {notification.message}
+                        </Text>
+                        <Text variant="small">{notification.user}</Text>
+                        <Text variant="small" styles={{ root: { color: "gray" } }}>
+                            {notification.time}
+                        </Text>
+                    </Stack>
+                </Stack>
+            ))}
+        </Stack>
+    );
+};
+
+export default NotificationList;
---
+++ MyNotifications/MyNotifications/index.ts
@@ -0,0 +1,43 @@
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
+import DataSetInterfaces = ComponentFramework.PropertyHelper.DataSetApi;
+type DataSet = ComponentFramework.PropertyTypes.DataSet;
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import  NotificationList  from "./NotificationList";
+
+export class MyNotifications implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+    private _container: HTMLDivElement;
+
+    constructor() {
+        // Empty
+    }
+
+    public init(
+        context: ComponentFramework.Context<IInputs>,
+        notifyOutputChanged: () => void,
+        state: ComponentFramework.Dictionary,
+        container: HTMLDivElement
+    ): void {
+        this._container = container;
+    }
+
+    public updateView(context: ComponentFramework.Context<IInputs>): void {
+        const notifications = [
+            { type: "Info", message: "This is an info message", user: "Allen Deu", time: "24 Nov 2018 at 9:30 AM", color: "green" },
+            { type: "Warning", message: "This is a warning message", user: "Darren", time: "24 Nov 2018 at 9:30 AM", color: "orange" },
+            { type: "Error", message: "This is an error message", user: "Arin Ganshiram", time: "24 Nov 2018 at 9:30 AM", color: "purple" },
+            { type: "Info", message: "This is another info message", user: "Jullet Den", time: "24 Nov 2018 at 9:30 AM", color: "blue" }
+        ];
+
+        var root = ReactDOM.createRoot(this._container);
+        root.render(React.createElement(NotificationList, { notifications }));
+    }
+
+    public getOutputs(): IOutputs {
+        return {};
+    }
+
+    public destroy(): void {
+
+    }
+}
