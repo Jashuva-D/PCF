21fcf7b0fc11f8eab1956c99f326f8f6eb6bf694
Jashuva Devireddy <jashuva52@gmail.com>
2026-03-23 00:09:43 +0530

    version 6

--- MyNotifications/MyNotifications/index.ts
+++ MyNotifications/MyNotifications/index.ts
@@ -25,10 +25,37 @@ export class MyNotifications implements ComponentFramework.StandardControl<IInpu
 
     public updateView(context: ComponentFramework.Context<IInputs>): void {
         const notifications = [
-            { icontype: 100000000, title: "Info Message", body: "This is an info message", createdon: new Date(), priority: 200000000 } as NotificationModel,
-            { icontype: 100000003, title: "Warning Message", body: "This is a warning message", createdon: new Date(), priority: 200000001 } as NotificationModel,
-            { icontype: 100000002, title: "Error Message", body: "This is an error message", createdon: new Date(), priority: 200000002 } as NotificationModel,
-            { icontype: 100000000, title: "Another Info Message", body: "This is another info message", createdon: new Date(), priority: 200000000 } as NotificationModel    
+            {
+                icontype: 100000000,
+                title: "Lorem ipsum dolor sit amet consectetur adipiscing",
+                body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+                createdon: new Date(),
+                priority: 200000000
+            } as NotificationModel,
+
+            {
+                icontype: 100000003,
+                title: "Ut enim ad minim veniam quis nostrud exercitation",
+                body: `Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+                createdon: new Date(),
+                priority: 200000001
+            } as NotificationModel,
+
+            {
+                icontype: 100000002,
+                title: "Sed ut perspiciatis unde omnis iste natus error",
+                body: `Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.`,
+                createdon: new Date(),
+                priority: 200000002
+            } as NotificationModel,
+
+            {
+                icontype: 100000000,
+                title: "Nemo enim ipsam voluptatem quia voluptas sit",
+                body: `Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.Sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.`,
+                createdon: new Date(),
+                priority: 200000000
+            } as NotificationModel
         ] as NotificationModel[];
 
         var root = ReactDOM.createRoot(this._container);
