3904e5c2a062ff27c029511396a89f2c73c6d0df
Jashuva Devireddy <jashuva52@gmail.com>
2026-03-30 22:48:54 +0530

    version 2

--- InquiryNotes/InquireyNotes/Notes.tsx
+++ InquiryNotes/InquireyNotes/Notes.tsx
@@ -105,7 +105,7 @@ class Notes extends React.Component<NotesProps, NotesState> {
     }
     Refresh() {
         var obj = this;
-        this.props.context.webAPI.retrieveMultipleRecords("cr549_inquirynotes",`?$filter=_cr549_ccifppifticket_value eq ${(this.props.context as any).page.entityId} $orderby=createdon desc`).then((resp) => {
+        this.props.context.webAPI.retrieveMultipleRecords("cr549_inquirynotes",`?$filter=_cr549_ccifppifticket_value eq '${(this.props.context as any).page.entityId}'&$orderby=createdon desc`).then((resp) => {
             let notes = [] as any[]
             resp.entities.forEach(x => {
                 notes.push({
