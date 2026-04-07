73d450345c71969763606f8fb145779ee6ced7bd
Jashuva Devireddy <jashuva52@gmail.com>
2026-04-07 21:31:04 +0530

    version 100

--- AppUserRoles/AppUserRoles/AppUserRoles.tsx
+++ AppUserRoles/AppUserRoles/AppUserRoles.tsx
@@ -361,20 +361,48 @@ class AppUserRoles extends React.Component<AppUserRolesProps, AppUserRolesState>
         });
         this.setState({items: items});
     }
-    onNewAppUserRole(){
+    async onNewAppUserRole(){
         var obj = this;
-        this.props.context.navigation.openForm({
-            entityName: "cr549_appuserrole",
-            useQuickCreateForm: true,
-            createFromEntity: {
-                id: (obj.props.context as any).page.entityId,
-                entityType: "cr549_application"
+        try {
+            var newappuserrole = await this.props.context.navigation.openForm({
+                entityName: "cr549_appuserrole",
+                useQuickCreateForm: true,
+                createFromEntity: {
+                    id: (obj.props.context as any).page.entityId,
+                    entityType: "cr549_application"
+                }
+            }).then(function (resp) {
+                return (resp.savedEntityReference && resp.savedEntityReference.length > 0) ? resp.savedEntityReference[0].id : null;
+            }, function (err) {
+                throw new Error(`error occured while creating the record, details: ${err?.message}`) ;
+            });
+
+            if (newappuserrole != null) {
+                var appuserrolerecord = await this.props.context.webAPI.retrieveRecord("cr549_appuserrole", newappuserrole!, "?$select=cr549_id,cr549_role,cr549_person").then(function (resp) { return resp; }, function (err) { throw new Error(`error occured while fetching the record, details: ${err?.message}`) });
+                // var personrecord = await this.props.context.webAPI.retrieveRecord("cr549_person", appuserrolerecord["cr549_person"]?.id.guid,"?$select=cr549_id,cr549_direct_phone,cr549_email_address,cr549_email_address_2,cr549_service_desk_agent").then(function(resp){ return resp;}, function(err){ throw new Error(`error occured while fetching the record, details: ${err?.message}`)});
+                var currentuserrecord = await this.props.context.webAPI.retrieveRecord("systemuser", this.props.context.userSettings.userId, "?$select=internalemailaddress").then(function (resp) { return resp; }, function (err) { throw new Error(`error occured while fetching the record, details: ${err?.message}`) });
+                var currentpersonrecord = await this.props.context.webAPI.retrieveMultipleRecords("cr549_person", `?$filter=cr549_email_address eq '${currentuserrecord["internalemailaddress"]}'&$select=cr549_id`).then(function (resp) { return resp.entities.length > 0 ? resp.entities[0] : null; }, function (err) { throw new Error(`error occured while fetching the record, details: ${err?.message}`); });
+                var rolerecord = await this.props.context.webAPI.retrieveRecord("cr549_role", appuserrolerecord["cr549_role"]?.id.guid, "?$select=cr549_id").then(function (resp) { return resp; }, function (err) { return null; });
+
+                obj.props.context.webAPI.createRecord("cr549_personupdatexwalk", {
+                    "cr549_pers_change_type": "added",
+                    "cr549_pers_update_method": "manual",
+                    "cr549_pers_updated_by": currentpersonrecord == null ? null : currentpersonrecord["cr549_id"],
+                    "cr549_pers_updated_date": new Date(),
+                    "cr549_pers_id_crmdb@odata.bind": `/cr549_persons(${appuserrolerecord["cr549_person"]?.id.guid})`,
+                    "cr549_role_id": rolerecord == null ? null : rolerecord["cr549_id"],
+                    "cr549_short_app_name@odata.bind": `/cr549_applications(${(obj.props.context as any).page.entityId})`
+                }).then(function (resp) {
+                    obj.showAlertMessage(CMSAlertType.Success, "Record updated successfully");
+                    obj.setState({ editablerecord: null });
+                    obj.props.context.parameters.sampleDataSet.refresh();
+                }, function (err) {
+                    throw new Error(`Error in updating record: ${err.message}`);
+                });
             }
-        }).then(function(resp){
-            if(resp.savedEntityReference && resp.savedEntityReference.length > 0){
-                obj.showAlertMessage(CMSAlertType.Success,"Record created successfully");
-            }
-        });
+        } catch (err) {
+            obj.showAlertMessage(CMSAlertType.Error, `error occured while creating the record, details: ${err instanceof Error ? err.message : String(err)}`);
+        }
     }
     onRefresh(){
         this.props.context.parameters.sampleDataSet.refresh();
