336fe9ac7d337544986d0735cd5a0fb167628c83
Jashuva Devireddy <jashuva52@gmail.com>
2026-04-25 01:40:41 +0530

    homepage version 14

--- HomePage/basecamp-homepage/src/PowerBiReport.tsx
+++ HomePage/basecamp-homepage/src/PowerBiReport.tsx
@@ -5,37 +5,55 @@ interface PowerBIReportProps {}
 interface PowerBIReportState {embedUrl: string}
 
 class PowerBIReport extends Component<PowerBIReportProps, PowerBIReportState> {
-  constructor(props: PowerBIReportProps) {
-    super(props);
-    this.state = {
-      embedUrl: "https://app.powerbigov.us/reportEmbed?reportId=0c535aed-3343-4710-8416-75dc1aab2bac&autoAuth=true&ctid=fbdcedc1-70a9-414b-bfa5-c3063fc3395e"
-    };
-  }
+    constructor(props: PowerBIReportProps) {
+        super(props);
+        this.state = {
+            embedUrl: ""
+        };
+    }
+    componentDidMount(): void {
+        var obj = this;
+        (parent as any).Xrm.WebApi.retrieveMultipleRecords("environmentvariabledefinition", `?$select=defaultvalue,schemaname&$filter=schemaname eq 'crm2_crmmainpage_reporturl'&$expand=environmentvariabledefinition_environmentvariablevalue($select=value)`).then(function (result: any) {
+            if (result.entities.length > 0) {
+                var reporturl = "";
+                let record = result.entities[0];
+                if (
+                    record.environmentvariabledefinition_environmentvariablevalue &&
+                    record.environmentvariabledefinition_environmentvariablevalue.length > 0 &&
+                    record.environmentvariabledefinition_environmentvariablevalue[0].value
+                ) {
+                    reporturl = record.environmentvariabledefinition_environmentvariablevalue[0].value;
+                }
+                reporturl = record.defaultvalue;
+            }
+            obj.setState({ embedUrl: reporturl! });
+        });
+    }
 
-  render() {
-      return <Stack tokens={{ childrenGap: 10 }}>
-          <Label style={{ fontWeight: "bold", fontSize: 16 }}>
-              Applications By Platform
-          </Label>
+    render() {
+        return <Stack tokens={{ childrenGap: 10 }}>
+            <Label style={{ fontWeight: "bold", fontSize: 16 }}>
+                Applications By Platform
+            </Label>
 
-          <div
-              style={{
-                  borderRadius: "6px",
-                  overflow: "hidden", // ✅ IMPORTANT
-                  border: "1px solid #ddd" // optional
-              }}
-          >
-              <iframe
-                  width="100%"
-                  height="600px"
-                  src={this.state.embedUrl}
-                  frameBorder="0"
-                  allowFullScreen
-                  style={{ display: "block" }} // avoids tiny gaps
-              />
-          </div>
-      </Stack>
-  }
+            <div
+                style={{
+                    borderRadius: "6px",
+                    overflow: "hidden", // ✅ IMPORTANT
+                    border: "1px solid #ddd" // optional
+                }}
+            >
+                <iframe
+                    width="100%"
+                    height="600px"
+                    src={this.state.embedUrl}
+                    frameBorder="0"
+                    allowFullScreen
+                    style={{ display: "block" }} // avoids tiny gaps
+                />
+            </div>
+        </Stack>
+    }
 }
 
 export default PowerBIReport;
