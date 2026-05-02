4d6ff1debc9d3d876f9d8a8024e8a2dad905aa63
Jashuva Devireddy <jashuva52@gmail.com>
2026-05-02 23:53:34 +0530

    homepage version 15

--- HomePage/basecamp-homepage/src/BaseCampLogo.tsx
+++ HomePage/basecamp-homepage/src/BaseCampLogo.tsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
 // import type { IStackStyles } from "@fluentui/react";
-import { Stack } from "@fluentui/react";
+//import { Stack } from "@fluentui/react";
 
 interface BaseCampLogoProps {}
 class BaseCampLogo extends Component<BaseCampLogoProps> {
@@ -13,20 +13,29 @@ class BaseCampLogo extends Component<BaseCampLogoProps> {
     //   },
     // };
 
-    return <Stack verticalAlign="center" horizontalAlign="center" horizontal styles={{root: {backgroundColor: "#AFCBE3",}}}>
-        <img
-            src="./basecamplogo.png"
-            alt="logo"
-            style={{
-              width: "100px",
-              height: "100px",
-              objectFit: "contain",
-            }}
-          />
-          <div style={{maxWidth: "600px", textAlign: "left", fontSize: 14, fontFamily: "Segoe UI", color: "#0D2499"}}>
-            Your centralized platform for managing applications, processes, reporting, and communication while connecting teams, data, and workflows in one seamless modern experience.
-          </div>
-    </Stack>
+    // return <Stack verticalAlign="center" horizontalAlign="center" horizontal styles={{root: {backgroundColor: "#AFCBE3",}}}>
+    //     <img
+    //         src="./basecamplogo.png"
+    //         alt="logo"
+    //         style={{
+    //           width: "100px",
+    //           height: "100px",
+    //           objectFit: "contain",
+    //         }}
+    //       />
+    //       <div style={{maxWidth: "600px", textAlign: "left", fontSize: 14, fontFamily: "Segoe UI", color: "#0D2499"}}>
+    //         Your centralized platform for managing applications, processes, reporting, and communication while connecting teams, data, and workflows in one seamless modern experience.
+    //       </div>
+    // </Stack>
+    return <img
+        src="./basecamplogo.png"
+        alt="logo"
+        style={{
+          width: "100%",
+          height: "200px",
+          objectFit: "contain",
+        }}
+      />  
   }
 }
 
