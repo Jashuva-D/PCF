6b42f367fd25d04275d9ee4523fd15079e58c9f0
Jashuva Devireddy <jashuva52@gmail.com>
2026-05-14 01:50:12 +0530

    version 6

--- HostingProject/HostingProject/LookupControl.tsx
+++ HostingProject/HostingProject/LookupControl.tsx
@@ -21,21 +21,21 @@ class LookupControl extends React.Component<LookupControlProps, LookupControlSta
         };
     }
     componentDidMount() {
-        // var obj = this;
-        // var recs : IPersonaProps[] = [];
-        // this.props.context.webAPI.retrieveMultipleRecords("cr549_projects", "?$select=cr549_projectnumber,cr549_projectnameshort").then(
-        //     (response) => {
-        //         response.entities.forEach((ent) => {
-        //             recs.push({ id: ent["cr549_projectsid"], text: ent["cr549_projectnameshort"], secondaryText: ent["cr549_projectnumber"], showSecondaryText: true } as IPersonaProps);
-        //         });
-        //         //var selectedrecords = response.entities.filter(x => x["cr549_projectnumber"] == this.props.hostingprojectnumber);
-        //         var selectedrecords = recs.filter(x => x.secondaryText == this.props.hostingprojectnumber);
-        //         obj.setState({ allitems: recs, selectedRecords: selectedrecords });
-        //     },
-        //     (error) => {
-        //         console.error("Error fetching records: ", error);
-        //     }
-        // );
+        var obj = this;
+        var recs : IPersonaProps[] = [];
+        this.props.context.webAPI.retrieveMultipleRecords("cr549_projects", "?$select=cr549_projectsid,cr549_projectnumber,cr549_projectnameshort").then(
+            (response) => {
+                response.entities.forEach((ent) => {
+                    recs.push({ id: ent["cr549_projectsid"], text: ent["cr549_projectnameshort"], secondaryText: ent["cr549_projectnumber"], showSecondaryText: true } as IPersonaProps);
+                });
+                //var selectedrecords = response.entities.filter(x => x["cr549_projectnumber"] == this.props.hostingprojectnumber);
+                var selectedrecords = recs.filter(x => x.secondaryText == this.props.hostingprojectnumber);
+                obj.setState({ allitems: recs, selectedRecords: selectedrecords });
+            },
+            (error) => {
+                console.error("Error fetching records: ", error);
+            }
+        );
         
     }
     
@@ -48,28 +48,28 @@ class LookupControl extends React.Component<LookupControlProps, LookupControlSta
     render() {
         const allitems = [...this.state.allitems];
         const header = "Projects";
-        return <div> Hosting Project </div>
-        // return (    
-        //     <NormalPeoplePicker
-        //         label="Hosting Project"
-        //         onEmptyResolveSuggestions={() => allitems}
-        //         onResolveSuggestions={this.onResolveSuggestions.bind(this)}
-        //         pickerSuggestionsProps={{
-        //             loadingText: "Loading...",
-        //             suggestionsHeaderText: header
-        //         }}
-        //         selectedItems={[...this.state.selectedRecords]}
-        //         itemLimit={1}
-        //         onChange={(items) => {
-        //             this.setState({selectedRecords: items ?? []})
-        //             if(items && items.length > 0) {
-        //                 var item = items[0];
-        //                 this.props.onRecordSelect(item.secondaryText as string);
-        //             }
-        //         }}
-        //         inputProps={{ style: { backgroundColor: 'white', width: 150 } }}
-        //     />
-        // );
+        //return <div> Hosting Project </div>
+        return (    
+            <NormalPeoplePicker
+                label="Hosting Project"
+                onEmptyResolveSuggestions={() => allitems}
+                onResolveSuggestions={this.onResolveSuggestions.bind(this)}
+                pickerSuggestionsProps={{
+                    loadingText: "Loading...",
+                    suggestionsHeaderText: header
+                }}
+                selectedItems={[...this.state.selectedRecords]}
+                itemLimit={1}
+                onChange={(items) => {
+                    this.setState({selectedRecords: items ?? []})
+                    if(items && items.length > 0) {
+                        var item = items[0];
+                        this.props.onRecordSelect(item.secondaryText as string);
+                    }
+                }}
+                inputProps={{ style: { backgroundColor: 'white', width: 150 } }}
+            />
+        );
     }   
 
 }
