de014584a156610d47ec83574284ae1816a03788
Jashuva Devireddy <jashuva52@gmail.com>
2026-08-01 21:31:39 +0530

    version 1

---
+++ Environment/Environment/EnvironmentTile.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { Icon, Stack, StackItem, initializeIcons } from "@fluentui/react";
+import {EnvironmentIcon, OpenInNewWindowIcon} from "./Icons";
+
+initializeIcons();
+
+interface EnvironmentTileProps {
+    environmentname: string;
+    applicationname: string;
+    color: string;
+}
+interface EnvironmentTileState {
+}
+
+class EnvironmentTile extends React.Component<EnvironmentTileProps, EnvironmentTileState>{
+    
+    constructor(props: EnvironmentTileProps) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <Stack horizontal verticalAlign="center" styles={{ root: { width: "100%", height: "100%" } }} horizontalAlign="space-between">
+                <>
+                <EnvironmentIcon size={64} color={this.props.color} />
+                <Stack verticalAlign="center" styles={{ root: { marginLeft: 10 } }}>
+                    <div style={{ fontSize: 24, color: this.props.color, fontWeight: "bold", alignContent: "start", alignItems: "start" }}>
+                        {this.props.environmentname}
+                    </div>
+                    <div style={{ fontSize: 16, color: this.props.color }} onClick={() => {alert('clicked')}}>
+                        Application URL: {this.props.applicationname}
+                    </div>
+                </Stack>
+                </>
+                <StackItem style={{ marginLeft: "auto", paddingRight: 10 }}><OpenInNewWindowIcon size={24} color={this.props.color} /></StackItem>
+            </Stack>
+        );
+    }
+}
+
+export default EnvironmentTile;
---
+++ Environment/Environment/index.ts
@@ -0,0 +1,39 @@
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
+import * as React from "react";
+import ReactDOM from "react-dom/client";
+import EnvironmentTile from "./EnvironmentTile";
+
+export class Environment implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+    
+    private _container: HTMLDivElement;
+    constructor() {
+        // Empty
+    }
+    
+    public init(
+        context: ComponentFramework.Context<IInputs>,
+        notifyOutputChanged: () => void,
+        state: ComponentFramework.Dictionary,
+        container: HTMLDivElement
+    ): void {
+        this._container = container;
+    }
+
+    public updateView(context: ComponentFramework.Context<IInputs>): void {
+        var root = ReactDOM.createRoot(this._container);
+
+        var environmentName = context.parameters.environmentname.raw || "DEV Environment";
+        var applicationName = context.parameters.applicationname.raw || "1115 PMDA";
+        var color = context.parameters.color.raw || "#0D2499";
+        
+        root.render(React.createElement(EnvironmentTile, { environmentname: environmentName, applicationname: applicationName, color: color }));
+    }
+
+    public getOutputs(): IOutputs {
+        return {};
+    }
+
+    public destroy(): void {
+        
+    }
+}
