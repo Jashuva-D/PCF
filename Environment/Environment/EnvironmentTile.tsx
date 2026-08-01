3409fc1d97347adbbf652511bc012f73299e833f
Jashuva Devireddy <jashuva52@gmail.com>
2026-08-02 00:53:38 +0530

    version 6

--- Environment/Environment/EnvironmentTile.tsx
+++ Environment/Environment/EnvironmentTile.tsx
@@ -89,14 +89,14 @@ class EnvironmentTile extends React.Component<EnvironmentTileProps, EnvironmentT
                 <Stack horizontal verticalAlign="center" styles={{ root: { width: "100%", height: "100%" } }} horizontalAlign="space-between">
                     <EnvironmentIcon size={48} color={this.props.color} />
                     <Stack verticalAlign="center" styles={{ root: { marginLeft: 10 } }}>
-                        <div style={{ fontSize: 20, color: this.props.color, fontWeight: "bold", alignContent: "start", alignItems: "start" }}>
+                        <div style={{ fontSize: 16, color: this.props.color, fontWeight: "bold", alignContent: "start", alignItems: "start" }}>
                             {this.props.environmentname}
                         </div>
-                        <div style={{ fontSize: 14, color: this.props.color }}>
-                            Application URL: {this.props.applicationname}
+                        <div style={{ fontSize: 12, color: this.props.color }}>
+                            <b>Application URL:</b> {this.props.applicationname}
                         </div>
                     </Stack>
-                    <StackItem style={{ marginLeft: "auto", paddingRight: 10 }} onClick={this.onClickOpenInNewWindow}><OpenInNewWindowIcon size={20} color={this.props.color} /></StackItem>
+                    <StackItem style={{ marginLeft: "auto", paddingRight: 10 }} onClick={this.onClickOpenInNewWindow}><OpenInNewWindowIcon size={16} color={this.props.color} /></StackItem>
                 </Stack>
             );
         }
