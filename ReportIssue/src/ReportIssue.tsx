a17795f7972de8d388d2a40291b856abb968d000
Jashuva Devireddy <jashuva52@gmail.com>
2026-08-13 21:16:11 +0530

    version 205

--- ReportIssue/src/ReportIssue.tsx
+++ ReportIssue/src/ReportIssue.tsx
@@ -136,7 +136,7 @@ export default class ReportIssue extends Component<ReportIssueProps, ReportIssue
           key: "currentvalue",
           name: "Current Value",
           fieldName: "CurrentValue",
-          minWidth: 200,
+          minWidth: 180,
           onRender: (item: any) => {
             if(item.newrecord){
               var currentvalue = (this.state.currentrecord?.fieldname != null && this.state.currentrecord?.fieldname != "" && (this.state.currentrecord?.currentvalue == null || this.state.currentrecord?.currentvalue == "")) ? "------" : this.state.currentrecord?.currentvalue; 
@@ -165,7 +165,7 @@ export default class ReportIssue extends Component<ReportIssueProps, ReportIssue
           key: "newvalue",
           name: "New Value",
           fieldName: "NewValue",
-          minWidth: 200,
+          minWidth: 180,
           onRender: (item: any) => {
             if(item.newrecord){
               var field = TabOptions.find(x => x.key == this.state.selectedTab)?.sections?.find(x => x.key == this.state.selectedSection)?.fields?.find(x => x.key == this.state.currentrecord?.fieldname);
