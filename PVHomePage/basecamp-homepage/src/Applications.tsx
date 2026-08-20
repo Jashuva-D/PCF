b27a079e0af15351f37a79383808d3c8b9e7dde5
Jashuva Devireddy <jashuva52@gmail.com>
2026-08-21 01:40:06 +0530

    version 24

--- PVHomePage/basecamp-homepage/src/Applications.tsx
+++ PVHomePage/basecamp-homepage/src/Applications.tsx
@@ -33,7 +33,7 @@ class Applications extends React.Component<MyApplicationsProps, MyApplicationsSt
                         return <Stack verticalAlign='center' horizontalAlign='start'><Link onClick={() => this.openRecord("pv_apps",item.key)} style={{fontSize: 14, fontWeight: 600, color: "#01395E"}}>{item.pv_id}</Link></Stack>;
                     }
                 },
-                { key: 'pv_businessownerofficecenter', name: 'Owner Group', fieldName: 'pv_businessownerofficecenter@OData.Community.Display.V1.FormattedValue', currentWidth: 100, minWidth: 100, maxWidth: 100, isResizable: true, 
+                { key: 'pv_businessownerofficecenter', name: 'Owner Group', fieldName: 'pv_businessownerofficecenter@OData.Community.Display.V1.FormattedValue', currentWidth: 120, minWidth: 120, maxWidth: 120, isResizable: true, 
                     onColumnClick: this.onColumnClick.bind(this),
                     onRender: (item: any) => {
                         if(item.pv_businessownerofficecenter){
@@ -42,7 +42,7 @@ class Applications extends React.Component<MyApplicationsProps, MyApplicationsSt
                         return null;
                     }
                 },
-                { key: 'pv_hostingplatform', name: 'Hosting Platform', fieldName: 'pv_hostingplatform@OData.Community.Display.V1.FormattedValue', currentWidth: 100, minWidth: 100, maxWidth: 100, isResizable: true, 
+                { key: 'pv_hostingplatform', name: 'Hosting Platform', fieldName: 'pv_hostingplatform@OData.Community.Display.V1.FormattedValue', currentWidth: 120, minWidth: 120, maxWidth: 120, isResizable: true, 
                     onColumnClick: this.onColumnClick.bind(this),
                     onRender: (item: any) => {
                         if(item.pv_hostingplatform){
