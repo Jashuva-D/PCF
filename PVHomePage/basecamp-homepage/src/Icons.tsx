17ebb33befa42d5a6172e0ac0612aead13ec9c53
Jashuva Devireddy <jashuva52@gmail.com>
2026-08-23 20:43:15 +0530

    version 30

--- PVHomePage/basecamp-homepage/src/Icons.tsx
+++ PVHomePage/basecamp-homepage/src/Icons.tsx
@@ -98,8 +98,6 @@ export const PowerVaultIcon: React.FC = () => {
   return (
   <svg xmlns="http://www.w3.org/2000/svg"
     viewBox="75 25 400 465"
-    width="100%"
-    height="100%"
   >
   <defs>
     <linearGradient id="outerDark" x1="0" y1="0" x2="1" y2="1">
